c4ab3c14db1db64988c592283f715699d1b68593
dophat1 <dophat2403@gmail.com>
2026-08-12 03:18:25 +0200

    create database for the process

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,22 @@
 CREATE TABLE companies (
-    company_id int PRIMARY KEY CONSTRAINT,
-    name varchar(255) NOT NULL,
-    career_page_url varchar(255),
+    company_id INTEGER PRIMARY KEY ,
+    name varchar(255) NOT NULL UNIQUE,
+    career_page_url varchar(255)
+);
+
+CREATE TABLE postings (
+    posting_id INTEGER PRIMARY KEY, 
+    company_id INTEGER REFERENCES companies(company_id),
+    source varchar(255) DEFAULT 'BA',
+    referenznummer varchar(255),
+    title varchar(255),
+    location varchar(255),
+    entfernung INTEGER,
+    publish_date date,
+    contract_type varchar(255),
+    raw_description_text text,
+    extracted_skills varchar(255), 
+    german_level_requirement varchar(255), 
+    salary INTEGER, 
+    UNIQUE(source, referenznummer)
 );

933992ab40843f1b440fcab1d57a1405410750e1
dophat1 <dophat2403@gmail.com>
2026-08-13 01:44:47 +0200

    load the data from json file to database

--- src/load_data.py
+++ src/load_data.py
@@ -1,4 +1,7 @@
 import sqlite3
+import db_connect
+from pathlib import Path
+import json
 
 def get_or_insert_company(conn, name):
     query = 'SELECT company_id FROM companies WHERE name = ?'
@@ -9,3 +12,37 @@ def get_or_insert_company(conn, name):
         add_query = 'INSERT INTO companies (name) VALUES (?)'
         update_db = conn.execute(add_query, (name,))
         return update_db.lastrowid
+
+# Insert 1 posting into postings
+def posting_loader(conn, data):
+    try:
+        company_id = get_or_insert_company(conn, data['firma'])
+        query = 'INSERT INTO postings (company_id, referenznummer, \
+                                        title, location, entfernung, publish_date, \
+                                        start_date, contract_type, raw_description_text,\
+                                        extracted_skills, german_level_requirement, salary) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)'
+        
+        values = (company_id, data['referenznummer'], data['stellenangebotsTitel'], data['stellenlokationen'][0]['adresse']['ort'], 
+                  data['entfernung'], data['datumErsteVeroeffentlichung'], data['eintrittszeitraum']['von'], 
+                  data['stellenangebotsart'], None, None, None, None
+        )
+        conn.execute(query, values)
+    except sqlite3.IntegrityError:
+        pass 
+
+# Orchestration 
+
+def run_load():
+    conn = db_connect.get_connection()
+    for file_path in Path("data/raw").glob("*.json"):
+        with open(file_path) as f:
+            full_json = json.load(f)
+            postings = full_json.get('ergebnisliste', [])
+            for posting in postings:
+                posting_loader(conn, posting)
+    
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    run_load()

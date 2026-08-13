c9abc37741c90509d4b906514fa919c087ecf266
dophat1 <dophat2403@gmail.com>
2026-08-13 23:59:08 +0200

    add count_location in dashboard

--- src/dashboard.py
+++ src/dashboard.py
@@ -9,6 +9,12 @@ def count_contract_type(conn):
     contract_type_count = {row[0]:row[1] for row in result}
     return contract_type_count
 
+def count_location(conn):
+    location_count_query = 'SELECT location, COUNT (*) FROM postings GROUP BY location'
+    result = conn.execute(location_count_query).fetchall()
+    location_count = {row[0]:row[1] for row in result}
+    return location_count
 
 conn = db_connect.get_connection()
 st.bar_chart(count_contract_type(conn))
+st.bar_chart(count_location(conn))

30eba51264821e69c4ce205f0db58830836de7b4
dophat1 <dophat2403@gmail.com>
2026-08-14 18:14:02 +0200

    correct the special character in umlaut before saving file in ingest_ba

--- src/ingest_ba.py
+++ src/ingest_ba.py
@@ -74,7 +74,7 @@ def save_raw(data: dict, tag: str) -> Path:
     
     filepath = RAW_DIR/filename
     
-    with open(filepath, 'w') as f: 
+    with open(filepath, 'w', encoding='utf-8') as f: 
         json.dump(data, f, indent=2, ensure_ascii=False)
 
     return filepath 

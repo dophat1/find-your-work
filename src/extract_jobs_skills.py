b7942b7d586b8b00d19bb8a35edcbdb13480ba1b
dophat1 <dophat2403@gmail.com>
2026-08-15 17:22:26 +0200

    create extract_jobs_skills.py, basic skeleton of skills dict

---
+++ src/extract_jobs_skills.py
@@ -0,0 +1,11 @@
+import re 
+
+SKILLS_DICTIONARY = {
+    'machine_learning':['machine learning', 'maschinelles lernen'],
+    'python':['python'],
+    'powerbi':['power bi'],
+    'statistics':['statistics', 'statistik', 'statistikmodell'],
+    'scikit_learn':['scikit-learn'],
+    'sql': ['sql'],
+    'docker':['docker']
+}
